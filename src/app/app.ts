8f33de3c00f62cc2699fba5a6959cf2033331978
Your Name <you@example.com>
2026-03-11 12:27:35 +0200

    first commait make design only

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { HomeProducts } from './components/home-products/home-products';
+import { Footer } from './components/footer/footer';
+import { Slider } from './components/slider/slider';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Navbar,HomeProducts,Footer,Slider],
   templateUrl: './app.html',
   styleUrl: './app.css'
+
 })
 export class App {
   protected readonly title = signal('e-commerce');
